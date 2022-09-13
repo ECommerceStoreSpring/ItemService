1e54dae168582b316d764a9f97ee43dfa3f8c920
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-09-13 16:56:38 +0530

    'changes'

--- src/main/java/com/ecommerce/itemservice/ItemServiceApplication.java
+++ src/main/java/com/ecommerce/itemservice/ItemServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketRestApplication {
+public class ItemServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketRestApplication.class, args);
+		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
 }
